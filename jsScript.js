353fe68e35880575215724bf72ee6daab18e21f5
Manotej Meka <manotejmeka@gmail.com>
2017-02-07 20:34:51 -0500

    Reload bug fixed

--- jsScript.js
+++ jsScript.js
@@ -11,7 +11,7 @@ pageVisitDiv.innerHTML = pageVisit
 
 window.localStorage.setItem('pageVisit', pageVisit)
 
-function themeChange() {
+function themeChange () {
   var button = document.getElementById('searchButton')
 
   var theme = window.localStorage.getItem('theme')
@@ -30,3 +30,21 @@ function themeChange() {
 
   window.localStorage.setItem('theme', theme)
 }
+
+var button = document.getElementById('searchButton')
+
+var theme = window.localStorage.getItem('theme')
+if (theme === null || theme === 'day-theme') {
+  theme = 'day-theme'
+  button.style.background = 'white'
+  button.style.color = '#6772e5'
+} else if (theme === 'night-theme') {
+  theme = 'night-theme'
+  button.style.background = 'gray'
+  button.style.color = '#acb2ef'
+}
+
+var bodyTag = document.getElementsByTagName('Body')[0]
+bodyTag.className = theme
+
+window.localStorage.setItem('theme', theme)
